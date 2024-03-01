41bb0bd5cd5fff855a23f3d3e2119bccbc394bb3
Larisa Ioana <larisaoltean27@outlook.com>
2024-03-01 08:42:48 +0100

    hovered images removed

--- components/tvSeries/tvSeries.js
+++ components/tvSeries/tvSeries.js
@@ -2,13 +2,11 @@ import { useState, useEffect } from 'react'
 import styles from './tvSeries.module.css';
 import { fetchAPI, IMAGE_URL_342 } from '@/utils/fetchFromAPI';
 import Image from 'next/image';
-import TvSeriesCard from '../tvSeriesCard/tvSeriesCard';
 import Loading from '../loading/loading';
 import { useTvShowsContext } from '@/context/tvSeriesContext';
 
 const TvSeries = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [hoveredId, setHoveredId] = useState(null);
 
   const { topRatedTvShows, setTopRatedTvShows } = useTvShowsContext();
 
@@ -31,14 +29,6 @@ const TvSeries = () => {
     fetchTvSeries(1);
   }, []);
 
-  const handleMouseEnter = (id) => {
-    setHoveredId(id);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredId(null);
-  };
-
   return (
     <>
       {!isLoading ?
@@ -47,12 +37,10 @@ const TvSeries = () => {
           <div className={styles.tvSeriesContainer}>
             {
               topRatedTvShows.results?.map((result) => {
-                const { id, poster_path, name, backdrop_path } = result;
+                const { id, poster_path, name } = result;
                 return <div key={id}>
                   <div
                     className={styles.imgContainer}
-                    onMouseEnter={() => handleMouseEnter(id)}
-                    onMouseLeave={handleMouseLeave}
                   >
                     {
                       poster_path &&
@@ -64,7 +52,6 @@ const TvSeries = () => {
                         loading="eager"
                       />
                     }
-                    {hoveredId === id && <TvSeriesCard name={name} image={backdrop_path} id={id} />}
                   </div>
                   <h2 className={styles.name}>{name}</h2>
                   {isLoading && <Loading />}
